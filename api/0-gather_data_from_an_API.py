351b175a982c2919bb9b2a4e1d52ae7a9fcef9ce
Your Name <you@example.com>
2023-02-24 13:30:16 +0300

    files updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,4 +25,3 @@ if __name__ == "__main__":
           task_com, total_task_done))
 
     [print("\t {}".format(task.get("title"))) for task in total_tasks]
-
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -34,10 +34,8 @@ def get_employee_ids():
 if __name__ == '__main__':
 
     employee_id = get_employee_ids()
-
     with open('todo_all_employees.json', "w") as f:
         all_users = {}
         for employee_id in employee_id:
             all_users[str(employee_id)] = get_employee_task(employee_id)
         f.write(json.dumps(all_users))
-        
